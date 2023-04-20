d78ee5bc92967c5c31cef68050424230da381cbb
Gabriel <gabrielmonteiroduete@gmail.com>
2023-04-19 22:55:05 -0300

    chore: configure setup

--- react-/src/App.js
+++ react-/src/App.js
@@ -1,14 +1,7 @@
 import "./App.css"
-import Home from "./views/home/Home"
-import HomeClass from "./views/home/HomeClass"
 
 function App() {
-  return (
-    <div className="App">
-      <Home msg="Oi sou o Componente Home #01" />
-      <HomeClass msg="Oi sou o Componente Home #02" />
-    </div>
-  )
+  return <main></main>
 }
 
 export default App
